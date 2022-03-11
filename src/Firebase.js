8358410945250e75c5a2a7b06aafa1418ede083c
Pedro Lucas <88797361+pelucs@users.noreply.github.com>
2022-03-11 19:25:35 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyC7cAaZNdz8U8hVvFYzpFadvloXnq3taoc",
-  authDomain: "task-manager-c8b28.firebaseapp.com",
-  projectId: "task-manager-c8b28",
-  storageBucket: "task-manager-c8b28.appspot.com",
-  messagingSenderId: "599424071101",
-  appId: "1:599424071101:web:e209e0a9e0beaaafc10ed4"
+  apiKey: "SUA CHAVE",
+  authDomain: "SUA CHAVE",
+  projectId: "SUA CHAVE",
+  storageBucket: "SUA CHAVE",
+  messagingSenderId: "SUA CHAVE",
+  appId: "SUA CHAVE"
 });
 
 const auth = firebase.auth();
